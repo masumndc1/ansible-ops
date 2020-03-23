92e5f4b161cd36ee32240edc69426afbb5d70e4d
Khabir Uddin <masumndc1@gmail.com>
2020-03-23 23:05:32 +0200

    giving an user sudo right without passwd typing

---
+++ gt.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+class GitOperation:
+
+  def __init__(self):
+    if len(sys.argv) == 2: 
+      self.msg = sys.argv[1]
+      GitOperation.GitPullPush(self.msg)
+
+    elif len(sys.argv) < 2: 
+      print('\033[91m' + "You want to pull updates from upstreams. Pulling update" + '\033[0m')
+      os.system('git pull')
+
+  @classmethod
+  def GitPullPush(cls, msg):
+    GitOperation.GitPrint()
+    os.system('git pull')
+    os.system ('git add .')
+    print('\033[91m' + "Commiting with msg: " + "'" + msg + "'" + '\033[0m')
+    os.system('git commit -m "%s"' % msg) 
+    print('\033[91m' + "Pushing now to upstream " + '\033[0m')
+    os.system('git push origin master')
+    GitOperation.GitPrint()
+
+  @classmethod
+  def GitPrint(cls):
+    cls.num = 70
+    #  for i in range(cls.num):
+    #  print("-" * i, end = '')
+    #  print("-" * cls.num, end = '')
+    print("-" * cls.num)
+
+GitOperation()
